050522bd69ae0339a2baa6a955c13052eef1138a
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-05-23 16:01:56 -0700

    got some cool stuff going on

--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -2,28 +2,39 @@ class View {
   constructor(game, containerEl) {
     this.game = game;
     this.containerEl = containerEl;
+    this.setupBoard();
+    containerEl.addEventListener("click", (this.handleClick));
   }
-  
+
   setupBoard() {
     let grid = document.createElement("ul");
     for (let row = 0; row < 3; row ++) {
       for (let col = 0; col < 3; col ++) {
         let ele = document.createElement("li");
-        ul.append(ele);
+        grid.appendChild(ele);
         const pos = [row, col];
-        ele.dataset.pos = pos;
+        ele.setAttribute("data-pos", pos);
       }
     }
-    grid.append(ul);
+    this.containerEl.appendChild(grid);
   }
-  
+
   handleClick(e) {
+    // click event handler that we pass to the event listener
+    // let square = e.target;
+    let pos = e.target.getAttribute("data-pos");
+    console.log(pos)
+    this.game.playMove(pos);
+    const mark = this.game.currentPlayer;
+    e.target.innerText = mark;
   }
 
   makeMove(square) {
+
   }
-  
+
   handleGameOver() {
+
   }
 }
 
